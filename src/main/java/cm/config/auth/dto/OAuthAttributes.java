8c82fb84d1291239417f1b4d6f9301249c54526f
Hosick <ghtjr5000@naver.com>
2020-01-18 10:26:53 +0900

    security update

--- src/main/java/cm/config/auth/dto/OAuthAttributes.java
+++ src/main/java/cm/config/auth/dto/OAuthAttributes.java
@@ -2,6 +2,7 @@ package cm.config.auth.dto;
 
 import cm.domain.user.Role;
 import cm.domain.user.User;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -25,6 +26,13 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -34,12 +42,24 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.USER)
+                .role(Role.GUEST)
                 .build();
     }
 }
