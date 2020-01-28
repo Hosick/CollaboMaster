d7115d3892917702cab88d9ce90dc52a19d0bbeb
Hosick <ghtjr5000@naver.com>
2020-01-28 22:04:42 +0900

    onmouse effect & date format apply

--- src/main/java/cm/web/dto/PostListResponseDto.java
+++ src/main/java/cm/web/dto/PostListResponseDto.java
@@ -4,19 +4,21 @@ import cm.domain.post.Post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class PostListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime createdDate;
+    private String createdDate;
 
     public PostListResponseDto(Post entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getUser().getName();
-        this.createdDate = entity.getCreatedDate();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        this.createdDate = entity.getCreatedDate().format(formatter);
     }
 
 }
